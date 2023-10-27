25774adb6e708fbda220e242b5cbd7b0dc6f7fd0
linmaoxu <303936532@qq.com>
2023-10-27 18:03:13 +0800

    <feature> add:pointer

---
+++ Pointer/Pointer/Pointer.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+class School {
+public:
+	int id;
+	int grade;
+	School() {
+		id = 1;
+		grade = 2;
+	}
+private:
+	int t;
+};
+
+int main() {
+	School m;
+	School* school = reinterpret_cast<School*>(&m);
+	cout << "id:" << school->id << endl;
+	cin.get();
+}
