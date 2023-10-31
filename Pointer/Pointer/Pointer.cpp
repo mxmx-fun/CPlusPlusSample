0728fecbc913d07d404d84f48cdcddb006a4d174
linmaoxu <303936532@qq.com>
2023-10-31 12:27:59 +0800

    <modify>指针和数组的关系

--- Pointer/Pointer/Pointer.cpp
+++ Pointer/Pointer/Pointer.cpp
@@ -1,21 +1,13 @@
 ﻿#include<iostream>
 using namespace std;
 
-class School {
-public:
-	int id;
-	int grade;
-	School() {
-		id = 1;
-		grade = 2;
-	}
-private:
-	int t;
-};
 
 int main() {
-	School m;
-	School* school = reinterpret_cast<School*>(&m);
-	cout << "id:" << school->id << endl;
+	//指针指向第一个地址，示例如下。
+	int a[3] = {1,2,3};
+	int* p = a;
+	//结果相等
+	cout << "id:" << a[1] << endl;
+	cout << "id:" << *(p+1) << endl;
 	cin.get();
 }
