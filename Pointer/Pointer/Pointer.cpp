9bb293a380ad6854556cd1855d12e288ae74ac76
linmaoxu <303936532@qq.com>
2023-10-31 15:06:14 +0800

    <feature> add:引用和指针

--- Pointer/Pointer/Pointer.cpp
+++ Pointer/Pointer/Pointer.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main() {
 	//指针指向第一个地址，示例如下。
 	int a[3] = {1,2,3};
-	int* p = a;
-	//结果相等
-	cout << "id:" << a[1] << endl;
+	int* p = &a[1];
+	int& t = a[1];
+	t = 3;
+	*p = 4;
+	//结果相等 
 	cout << "id:" << *(p+1) << endl;
+	cout << "id:" << t << endl;
+	cout << "id:" << a[1] << endl;
 	cin.get();
 }
